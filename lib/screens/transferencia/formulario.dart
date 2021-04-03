5b9eb400807d050eaa063bdd670b62a9fb6f05d0
wodejunior <wodejunior@gmail.com>
2021-04-03 12:47:04 -0300

    Projeto Final
    Signed-off-by: wodejunior <wodejunior@gmail.com>

---
+++ lib/screens/transferencia/formulario.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencia.dart';
+
+const _tituloAppBar = 'Criando Transferência';
+const _rotuloCampoNumeroConta = 'Número da Conta';
+const _dicaCampoNumeroConta = '000';
+const _rotuloCampoValor = 'Valor Transferência';
+const _dicaCampoValor = '0.00';
+const _botaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(_tituloAppBar)),
+        body: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: _rotuloCampoNumeroConta,
+                dica: _dicaCampoNumeroConta),
+            Editor(
+                controlador: _controladorCampoValor,
+                rotulo: _rotuloCampoValor,
+                dica: _dicaCampoValor,
+                icone: Icons.monetization_on),
+            RaisedButton(
+              child: Text(_botaoConfirmar),
+              onPressed: () => _criaTransferencia(context),
+            )
+          ],
+        )));
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
