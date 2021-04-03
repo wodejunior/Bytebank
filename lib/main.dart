339b87d17e0d75f44701f68de0029523d7a68cb1
wodejunior <wodejunior@gmail.com>
2021-04-03 11:23:36 -0300

    Atualizaçã0 05
    Signed-off-by: wodejunior <wodejunior@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,13 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-      ),
-    );
+        theme: ThemeData(
+            primaryColor: Colors.green[900],
+            accentColor: Colors.blueAccent[700],
+            buttonTheme: ButtonThemeData(
+                buttonColor: Colors.blueAccent[700],
+                textTheme: ButtonTextTheme.primary)),
+        home: ListaTransferencias());
   }
 }
 
